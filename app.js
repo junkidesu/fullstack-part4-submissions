4d3cfe7a0f64094182b91c06de9b5356aaf8fad8
Anwar Sh <alvaro.sh03@gmail.com>
2022-08-11 17:41:54 +0500

    Refactor the app into its own module

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+
+console.log('connecting to', process.env.MONGODB_URI)
+
+mongoose.connect(mongoUrl)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+module.exports = app
