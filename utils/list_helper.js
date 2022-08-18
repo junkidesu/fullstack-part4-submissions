aa53217b79ae5e4b8369b5b68d2073ba0d5c852a
Anwar Sh <alvaro.sh03@gmail.com>
2022-08-18 18:27:22 +0500

    Add totalLikes function to list_helper

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.map(b => b.likes)
+        .reduce((prev, curr) => prev + curr, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
