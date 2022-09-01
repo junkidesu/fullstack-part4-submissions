52947c2de356f060ff1a58a7f370dd9624982523
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-01 13:56:37 +0500

    Write test for number of blogs returned

---
+++ tests/blog_api.test.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+const app = require('../app')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7
+    },
+    {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5
+    },
+    {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12
+    },
+    {
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10
+    },
+    {
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0
+    },
+    {
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = initialBlogs.map(b => new Blog(b))
+
+    const promiseArray = blogObjects.map(b => b.save())
+    await Promise.all(promiseArray)
+}, 10000)
+
+test('correct number of blogs is returned in JSON format', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(6)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
