54bfb81c804123741735b9042d7ec5791f62032f
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-01 15:55:32 +0500

    Refactor POST /api/blogs endpoint

--- constollers/blogs.js
+++ constollers/blogs.js
@@ -7,14 +7,11 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
