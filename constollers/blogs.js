676dc352aff5a33e43641e3d4c8761b17a9300ea
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-01 16:16:55 +0500

    Likes property defaults to 0 if missed

--- constollers/blogs.js
+++ constollers/blogs.js
@@ -8,7 +8,11 @@ blogsRouter.get('/', async (request, response) => {
 
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
+    const blog = new Blog({
+        ...body,
+        likes: body.likes || 0
+    })
 
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
