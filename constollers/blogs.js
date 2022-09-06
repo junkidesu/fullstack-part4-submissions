3dbdb72287276b23bc1a515ea4a5fe571f9ec455
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-06 18:40:56 +0500

    GET /api/blogs and /api/users use populate

--- constollers/blogs.js
+++ constollers/blogs.js
@@ -4,7 +4,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
     response.json(blogs)
 })
 
