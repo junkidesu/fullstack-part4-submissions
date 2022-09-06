57ffce987507d928be59525fd6e831dac5181413
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-06 18:16:56 +0500

    Limit creation of blogs to logged in users

--- constollers/blogs.js
+++ constollers/blogs.js
@@ -1,20 +1,45 @@
 const blogsRouter = require('express').Router()
-const Blog = require('./../models/blog')
+const jwt = require('jsonwebtoken')
+const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+
+    return null
+}
 
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
+
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.decode(token, process.env.SECRET)
+    if (!decodedToken || !decodedToken.id) {
+        return response.status(401).send({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
     const blog = new Blog({
         ...body,
-        likes: body.likes || 0
+        likes: body.likes || 0,
+        user: user._id
     })
 
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
