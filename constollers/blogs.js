29fd2f1a57b2b95cc8a1b57c116ae676bc3028d2
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-06 19:17:14 +0500

    Implement token extractor middleware

--- constollers/blogs.js
+++ constollers/blogs.js
@@ -8,20 +8,10 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-
-    return null
-}
-
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const token = getTokenFrom(request)
+    const token = request.token
     const decodedToken = jwt.decode(token, process.env.SECRET)
     if (!decodedToken || !decodedToken.id) {
         return response.status(401).send({ error: 'token missing or invalid' })
--- utils/middleware.js
+++ utils/middleware.js
@@ -14,6 +14,17 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7)
+    }
+
+    next()
+}
+
 module.exports = {
-    errorHandler
+    errorHandler,
+    tokenExtractor
 }
