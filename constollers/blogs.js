d1f1a0dfeb5d9e4a91f7bd6a5fb330cde6ecdcd2
Anwar Sh <alvaro.sh03@gmail.com>
2022-09-13 12:10:05 +0500

    Refactor POST /api/blogs request

--- constollers/blogs.js
+++ constollers/blogs.js
@@ -28,6 +28,8 @@ blogsRouter.post('/', async (request, response, next) => {
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
 
+    await savedBlog.populate('user', { username: 1, name: 1 })
+
     response.status(201).json(savedBlog)
 })
 
